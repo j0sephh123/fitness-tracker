fb6c3690b796d4d30925255d9c73f89cf45029c1
j0sephh123 <iossif123gg@gmail.com>
2023-02-25 14:43:22 +0200

    fix mobile issue

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex gap-5">
-      <Link className="text-white" href="/">Home</Link>
-      <Link className="text-white" href="/workouts">Workouts</Link>
-      <Link className="text-white" href="/workouts/create">Create a workout</Link>
-      <Link className="text-white" href="/exercises">Exercises</Link>
-      <Link className="text-white" href="/exercises/create">Create an exercise</Link>
+    <nav className="flex flex-wrap gap-5 border-b-white border-b">
+      <Link className="text-white" href="/">
+        Home
+      </Link>
+      <Link className="text-white" href="/workouts">
+        Workouts
+      </Link>
+      <Link className="text-white" href="/exercises">
+        Exercises
+      </Link>
     </nav>
   );
 }
